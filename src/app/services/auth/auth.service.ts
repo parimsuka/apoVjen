7d784ff9252d51e364b45417a3775dd0cf277c7b
Parim Suka <iloftp@inf.elte.hu>
2022-03-07 01:13:31 +0100

    Added firebase database to the project

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,40 +1,39 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/model/user/User';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import * as firebase from '../../../../node_modules/firebase/compat';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private auth: AngularFireAuth) { }
 
   recoverEmailPassword(email: string) : Observable<void> {
     return new Observable<void>(observer => {
-      setTimeout(() => {
-        if(email == "error@email.com") {
-          observer.error({message: "Email not found"});
-        }
+      this.auth.sendPasswordResetEmail(email).then(() => {
         observer.next();
         observer.complete();
-      }, 3000);
+      }).catch(error => {
+        observer.error(error);
+        observer.complete();
+      })
     })
   }
 
   login(email: string, password: string) : Observable<User> {
     return new Observable<User>(observer => {
-      setTimeout(() => {
-        if(email == "error@email.com") {
-          observer.error({message: "User not found"});
-          observer.next();
-        } else {
-          const user = new User();
-          user.email = email;
-          user.id = "userId";
-          observer.next(user);
-        }
-        observer.complete();
-      }, 3000);
+      this.auth.setPersistence(firebase.default.auth.Auth.Persistence.LOCAL).then(() => {
+        this.auth.signInWithEmailAndPassword(email, password).then((firebaseUser: firebase.default.auth.UserCredential) => {
+          observer.next({email, id: firebaseUser.user.uid});
+          observer.complete();
+        }).catch(error => {
+          observer.error(error);
+          observer.complete();
+        })
+      })
     })
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyDW5YFZCfJWupA9lJH5M_7whKkcwriam2A",
+    authDomain: "apovjen-c6238.firebaseapp.com",
+    projectId: "apovjen-c6238",
+    storageBucket: "apovjen-c6238.appspot.com",
+    messagingSenderId: "589864188519",
+    appId: "1:589864188519:web:d23be4b4b8438cd4adb2c2"
+  },
   production: false
 };
 
