698ca7a21684e596aeeb5c055b66ebdda6ec51ff
Parim Suka <iloftp@inf.elte.hu>
2022-05-19 00:38:44 +0200

    Added review form and the call to backend

--- src/app/pages/user-profile/user-profile.module.ts
+++ src/app/pages/user-profile/user-profile.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -18,6 +18,7 @@ import { ReviewFormComponent } from 'src/app/components/review-form/review-form.
     FormsModule,
     IonicModule,
     UserProfilePageRoutingModule,
+    ReactiveFormsModule,
     TranslateModule.forChild({
       loader: {
         provide: TranslateLoader,
